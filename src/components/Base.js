b5d069b093c26bd579ec10861513793e0fe6d964
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-05 22:55:46 -0700

    add variants and keyframes

--- src/components/Base.js
+++ src/components/Base.js
@@ -2,15 +2,55 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw'
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: 'spring',
+      delay: 0.5
+    }
+  }
+}
+
+const nextVariants = {
+  hidden: {
+    x: '-100vw'
+  },
+  visible: {
+    x: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 120
+    }
+  }
+}
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1, 
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.4,
+      repeat: Infinity 
+    }
+  }
+}
+
 const Base = ({ addBase, pizza }) => {
   const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
 
   return (
     <motion.div className="base container"
-      initial={{ x: '100vw'}}
-      animate={{ x: 0 }}
-      transition={{ type: 'spring', delay: 0.5 }}>
-
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
         {bases.map(base => {
@@ -28,16 +68,12 @@ const Base = ({ addBase, pizza }) => {
 
       {pizza.base && (
         <motion.div className="next"
-          initial={{ x: '-100vw' }}
-          animate={{ x: 0 }}
-          transition={{ type: 'spring', stiffness: 120}}
+        variants={nextVariants}
         >
           <Link to="/toppings">
             <motion.button
-              whileHover={{ 
-                scale: 1.1, 
-                textShadow: "0px 0px 8px rgb(255,255,255)",
-                boxShadow: "0px 0px 8px rgb(255,255,255)" }} 
+              variants={buttonVariants}
+              whileHover="hover" 
             > 
               Next
             </motion.button>
