a5019b48019b1e7009b8a428c387fe18bc615c63
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-05 20:59:58 -0700

    add home button

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Order = ({ pizza }) => {
   return (
@@ -6,7 +7,14 @@ const Order = ({ pizza }) => {
       <h2>Thank you for your order</h2>
       <p>You ordered a {pizza.base} pizza with:</p>
       {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
+
+      <Link to="/">
+      <button>
+        Back to Home
+      </button>
+      </Link>
     </div>
+
   )
 }
 
